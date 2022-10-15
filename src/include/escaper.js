fb6a04b906b8b1c22107eefaa95b59549c5bcdd8
HappyKimoto <seiji.kimoto@screen-spe.com>
2022-10-15 13:37:30 -0700

    Added Escape functions
    Added VBS-UnescapeQuotes
    Added VBS-LinesToJoinedArray
    Added XML-Rev

--- src/include/escaper.js
+++ src/include/escaper.js
@@ -9,33 +9,53 @@ function EscChr(){
 		txt_out = txt_out.replaceAll("\"", "\"\"");
 	}
 
-
-
+	// | Original character | Escaped character | 
+	// | ------------------ | ----------------- | 
+	// | "                  | &quot;            | 
+	// | '                  | &apos;            | 
+	// | <                  | &lt;              | 
+	// | >                  | &gt;              | 
+	// | &                  | &amp;             | 
+	// Order: Ampersand needs to be replaced first for escaping.
+	let escape_array = [
+		["&", "&amp;"],
+		["\"", "&quot;"],
+		["'", "&apos;"],
+		["<", "&lt;"],
+		[">", "&gt;"],
+	];
 
 	switch (esc_for) {
-		case "VBS-DoubleQuotes":
+		case "VBS-EscapeQuotes":
 			txt_out = txt_out.replaceAll("\"", "\"\"");
 			break;
+		case "VBS-UnescapeQuotes":
+			txt_out = txt_out.replaceAll("\"\"", "\"");
+			break;
+		case "VBS-LinesToJoinedArray":
+			// escape double quotes
+			txt_out = txt_out.replaceAll("\"", "\"\"");
+			// create lines
+			let lines = txt_out.split("\n")
+			// initialize text output
+			txt_out = "Join(Array( _\n"
+			// loop through lines before the last line
+			for (let i = 0; i < lines.length - 1; i++){
+				txt_out += "\"" + lines[i] + "\", _" + "\n"
+			}
+			// add last line and closing
+			txt_out += "\"" + lines[lines.length - 1] +"\"), vbCrlf)"
+			break;
 		case "XML":
-			// | Original character | Escaped character | 
-			// | ------------------ | ----------------- | 
-			// | "                  | &quot;            | 
-			// | '                  | &apos;            | 
-			// | <                  | &lt;              | 
-			// | >                  | &gt;              | 
-			// | &                  | &amp;             | 
-			// Order: Ampersand needs to be replaced first!
-			let escape_array = [
-				["&", "&amp;"],
-				["\"", "&quot;"],
-				["'", "&apos;"],
-				["<", "&lt;"],
-				[">", "&gt;"],
-			];
 			for (let i = 0; i < escape_array.length; i++){
 				txt_out = txt_out.replaceAll(escape_array[i][0], escape_array[i][1]);
 			}
 			break;
+		case "XML-Rev":
+				for (let i = 0; i < escape_array.length; i++){
+					txt_out = txt_out.replaceAll(escape_array[i][1], escape_array[i][0]);
+				}
+				break;
 		case "RegExp-Parentheses":
 			txt_out = txt_out.replaceAll("(", "\\(");
 			txt_out = txt_out.replaceAll(")", "\\)");
